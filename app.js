1914f2052e763ef5333775d37f390a84d17a1213
Mrinmoy09 <mrinmoy0908@gmail.com>
2021-09-02 03:59:42 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const searchText = document.getElementById('search-field');
+const booksContainer = document.getElementById('book-container');
+const totalResult = document.getElementById('total-result');
+
+// fetch data
+const loadData = () => {
+    const url = `http://openlibrary.org/search.json?q=${searchText.value}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayBooks(data))
+}
+
+
+// display books 
+const displayBooks = data => {
+    
+    // clear results
+    booksContainer.textContent='';
+    searchText.value ='';
+    totalResult.innerHTML = '';
+    const books = data.docs;
+    totalResult.classList.remove('d-none');
+    if(books.length === 0){
+      const section = document.createElement('section');
+      section.innerHTML = `
+      <h2 class="text-center text-white">Total results: No result Found</h2>
+    `;
+      totalResult.appendChild(section);
+    }
+    else {
+      const section = document.createElement('section');
+      section.innerHTML = `
+      <h2 class="text-center text-white">Total results: ${data.numFound}</h2>
+    `;
+      totalResult.appendChild(section);
+    }
+    
+    books.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('col-md-3');
+        div.innerHTML = `
+         <div class="card h-100 my-3 p-3">
+            <img src='https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg' class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${book.text[1]}</h5>
+              <p class="card-text">Author Name: ${book.author_name[0]}</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Originally Published: ${book.publish_date[0]}</small>
+              <br>
+              <small class="text-muted">Publisher: ${book.publisher[0]}</small>
+            </div>
+          </div>
+    `;
+    booksContainer.appendChild(div);
+  });
+}
+
---
+++ notes.md
@@ -0,0 +1,15 @@
+Assignment 6 requirement:
+
+
+
+<!-- 1- get the search result using api -->
+<!-- 1- show the search result (cover(bonus) , author name ,first published, total results) -->
+<!-- 2- no result found message  -->
+
+Bonus marks 
+<!-- 1- use arrow function -->
+<!-- 2-foreach loop -->
+<!-- 3- triple equal -->
+
+Repo link : https://github.com/Programming-Hero-Web-Course4/book-archive-Mrinmoy09
+API link : http://openlibrary.org/search.json?q=$%7BsearchText%7D
